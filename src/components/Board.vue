d5ad3a1f24d68975ea9207e74bf57aa6c50ca5c1
Noah <noahramey1997@gmail.com>
2017-03-28 11:52:42 -0700

    added Board and Cell components and added styling to all components

---
+++ src/components/Board.vue
@@ -0,0 +1,41 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data () {}
+  }
+</script>
+
+<style>
+  .grid {
+  background-color: #34495e;
+  color: #fff;
+  width: 100%;
+  border-collapse: collapse;
+  }
+
+  .gameStatus {
+  margin: 0px;
+  padding: 15px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: #f1c40f;
+  color: #fff;
+  font-size: 1.4em;
+  font-weight: bold;
+  }
+
+  .statusTurn {
+    background-color: #f1c40f;
+  }
+
+  .statusWin {
+    background-color: #2ecc71;
+  }
+
+  .statusDraw {
+    background-color: #9b59b6;
+  }
+</style>
